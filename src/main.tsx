1254421afc866db2a8a8fe3aa8fb8028c15dfe5e
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2025-08-14 00:26:08 +0530

    Enh: Move styles to folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
 import Overlay from './components/Overlay.tsx';
-import './index.css';
+import './css/index.css';
 
 const rootElement = document.getElementById('root');
 
